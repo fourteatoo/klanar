c4b79ce04bdc41ef01b488444af90705760296bd
Walter Pelissero <walter@pelissero.de>
2026-01-14 15:41:51 +0100

    Add misc.clj

---
+++ src/fourteatoo/klanar/misc.clj
@@ -0,0 +1,28 @@
+(ns fourteatoo.klanar.misc
+  (:require
+   [fourteatoo.klanar.log :as log]
+   [diehard.core :as dh]
+   [clojure.string :as s]))
+
+
+(def exit? (promise))
+
+(defn exiting? []
+  (realized? exit?))
+
+(defn wait-exit []
+  (deref exit?))
+
+(defn arm-exit-hooks []
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. (fn []
+                               (log/info "Shutting down")
+                               (println "Exiting...")
+                               (deliver exit? true)))))
+
+(dh/defretrypolicy retry-policy
+  {:max-retries 10
+   :backoff-ms [(* 3 1000) (* 60 1000)]})
+
+(defn expand-home-dir [s]
+  (s/replace-first s #"^~/" (str (System/getProperty "user.home") "/")))
