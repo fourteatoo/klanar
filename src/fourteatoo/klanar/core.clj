32a2211accbe274765e1f3ed00545ec7e2016353
Walter Pelissero <walter@pelissero.de>
2026-01-14 13:45:31 +0100

    Refactoring

--- src/fourteatoo/klanar/core.clj
+++ src/fourteatoo/klanar/core.clj
@@ -9,15 +9,18 @@
             [clojure.java.io :as io])
   (:gen-class))
 
-(defn- log-message [msg]
-  (log/debug "msg:" (mail/get-message-id (:message msg)))
-  msg)
+
+(defn- filter-renewals [messages]
+  (remove (comp nil? :renew-link) messages))
+
+(defn- convert-messages [messages]
+  (map mail/parse-message messages))
 
 (defn- search-renew-messages [inbox search]
   (->> (imap/folder-search inbox (merge (conf :search)
                                         search))
-       (map mail/parse-message)
-       (remove (comp nil? :renew-link))))
+       convert-messages
+       filter-renewals))
 
 (defn- messages-move [inbox messages outbox]
   (log/debug "moving" (count messages) "messages from" inbox "to" outbox)
@@ -31,39 +34,59 @@
   (log/debug "marking as seen" (count messages) "messages from" inbox)
   (imap/messages-mark-as-seen inbox (map :message messages)))
 
-(defn- ensure-folder [store name]
-  (let [folder (imap/get-folder imap/store (conf :after-processing :move-to))]
+(defn- ensure-folder [folder]
+  (let [folder (imap/as-folder imap/store folder)]
     (when-not (imap/folder-exists? folder)
       (imap/folder-create folder))
     folder))
 
-(defn- process-messages [f & [search]]
+(defn- open-inbox []
+  (imap/open-folder imap/store
+                    (or (conf :imap :folder)
+                        "INBOX")))
+
+(defn- dispose-of-processed-messages [inbox messages]
+  (when-not (opt :dry-run)
+    (log/debug "disposing of" (count messages) "messages")
+    (let [outbox (when (conf :after-processing :move-to)
+                   (ensure-folder (conf :after-processing :move-to)))]
+      (when (conf :after-processing :mark-as-seen)
+        (messages-mark-as-seen inbox messages))
+      ;; either move or delete
+      (cond (conf :after-processing :move-to)
+            (messages-move inbox messages outbox)
+            (conf :after-processing :delete)
+            (messages-delete inbox messages)))))
+
+(defn- process-batch [messages inbox]
+  (log/info "processing" (count messages) "messages")
+  (doseq [msg messages]
+    (log/debug "msg:" (mail/get-message-id (:message msg)))
+    (log/info "ad" (get-in msg [:ad :title]) "is expiring"
+              (str "(" (get-in msg [:ad :href]) ")"))
+    (when-not (opt :dry-run)
+      (http/extend-ad (:renew-link msg))))
+  (dispose-of-processed-messages inbox messages))
+
+(defn- process-last-period [& [search]]
   (log/info "process messages" (str search))
-  (with-open [inbox (imap/open-folder imap/store (or (conf :imap :folder)
-                                                     "INBOX"))]
-    (let [messages (search-renew-messages inbox search)
-          outbox (when (conf :after-processing :move-to)
-                   (ensure-folder imap/store (conf :after-processing :move-to)))]
-      (log/info "processing" (count messages) "messages")
-      (->> messages
-           (map log-message)
-           (run! f))
-      (when-not (opt :dry-run)
-        (when (conf :after-processing :mark-as-seen)
-          (messages-mark-as-seen inbox messages))
-        ;; either move or delete; not both
-        (cond (conf :after-processing :move-to)
-              (messages-move inbox messages outbox)
-              (conf :after-processing :delete)
-              (messages-delete inbox messages))))))
+  (with-open [inbox (open-inbox)]
+    (let [messages (search-renew-messages inbox search)]
+      (process-batch messages inbox))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn- file-exists? [f]
+  (.exists f))
+
 (def ^:private cli-options
   [["-c" "--config FILE" "use confirguration file"
-    :parse-fn #(io/file %)
-    :validate [#(.exists %) "Configuration file does not exist"]]
+    :parse-fn io/file
+    :validate [file-exists? "Configuration file does not exist"]]
+   ["-d" "--days N" "process messages from the last N days"
+    :parse-fn #(Integer/parseUnsignedInt %)]
    ["-n" "--dry-run" "don't actually renew ads"]
+   ["-m" "--monitor" "enter monitor mode"]
    ["-l" "--list-folders" "list all folders on the server"]
    ["-v" "--verbose" "increase logging verbosity"
     :default 0
@@ -86,15 +109,30 @@
     result))
 
 (defn- renew-ads []
-  (process-messages (fn [msg]
-                      (log/info "ad" (get-in msg [:ad :title]) "is expiring"
-                                (str "(" (get-in msg [:ad :href]) ")"))
-                      (when-not (opt :dry-run)
-                        (http/extend-ad (:renew-link msg))))))
+  (process-last-period {:days (or (opt :days) 7)}))
 
 (defn- list-folders []
   (run! (comp println imap/folder-full-name)
         (imap/list-folders imap/store)))
+
+(defn- handle-mailbox-event [e inbox]
+  (-> (imap/event-messages e)
+      convert-messages
+      filter-renewals
+      (process-batch inbox)))
+
+(defn- monitor-mailbox []
+  (log/info "entering monitor mode")
+  (with-open [inbox (open-inbox)]
+    (imap/add-message-count-listener inbox
+                                     :added handle-mailbox-event)
+    (loop []
+      ;; the listener gets events only when we do something with the
+      ;; API, so we need to check if the mailbox is open once in a
+      ;; while.
+      (Thread/sleep (* (or (conf :poll-period) 3) 1000))
+      (imap/folder-open? inbox)
+      (recur))))
 
 (defn -main [& args]
   (let [{:keys [options summary]} (parse-cli args)]
@@ -102,6 +140,8 @@
       (mount/start)
       (cond (:help options)
             (usage summary nil)
+            (:monitor options)
+            (monitor-mailbox)
             (:list-folders options)
             (list-folders)
             :else
